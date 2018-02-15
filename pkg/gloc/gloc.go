bac4b95b08ae78eb727519d063174c629ec6e3ce
Michael Hrivnak <mhrivnak@hrivnak.org>
2018-02-14 22:43:00 -0500

    parses json into latitude and longitude

--- pkg/gloc/gloc.go
+++ pkg/gloc/gloc.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"github.com/tidwall/gjson"
 	"golang.org/x/net/html"
 	"golang.org/x/net/publicsuffix"
 	"io"
@@ -11,6 +12,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -18,6 +20,15 @@ const loginURL = "https://accounts.google.com/ServiceLogin"
 const emailURL = "https://accounts.google.com/signin/v1/lookup"
 const passwordURL = "https://accounts.google.com/signin/challenge/sl/password"
 const locationURL = "https://www.google.com/maps/preview/locationsharing/read?gl=en&pb=%211m7%218m6%211m3%211i14%212i8413%213i5385%212i6%213x4095%212m3%211e0%212sm%213i407105169%213m7%212sen%215e1105%2112m4%211e68%212m2%211sset%212sRoadmap%214e1%215m4%211e4%218m2%211e0%211e1%216m9%211e12%212i2%2126m1%214b1%2130m1%211f1.3953487873077393%2139b1%2144e1%2150e0%2123i4111425&authuser=0&hl=en"
+
+type Location struct {
+	Latitude  float64
+	Longitude float64
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf("Lat: %f, Lon: %f", l.Latitude, l.Longitude)
+}
 
 func Get(username, password string) error {
 	client, err := login(username, password)
@@ -39,9 +50,32 @@ func Get(username, password string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(b))
+
+	sdata := strings.SplitN(string(b), "'", 2)
+	if len(sdata) != 2 {
+		return errors.New("separator not found")
+	}
+
+	position, err := jsonToLocation(sdata[1])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(position.String())
 
 	return nil
+}
+
+func jsonToLocation(data string) (Location, error) {
+	parsed := gjson.Parse(data)
+	position := Location{
+		Latitude:  parsed.Get("0.0.1.1.2").Num,
+		Longitude: parsed.Get("0.0.1.1.1").Num,
+	}
+	if position.Latitude == 0 || position.Longitude == 0 {
+		return position, errors.New("could not find value in json")
+	}
+	return position, nil
 }
 
 func newClient() (*http.Client, error) {
