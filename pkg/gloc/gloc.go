f51964e4f8ac264f9077472c4a3f6c94fbff6b5e
Michael Hrivnak <mhrivnak@hrivnak.org>
2018-02-15 23:31:37 -0500

    finds all available results, not just the first one

--- pkg/gloc/gloc.go
+++ pkg/gloc/gloc.go
@@ -22,12 +22,13 @@ const passwordURL = "https://accounts.google.com/signin/challenge/sl/password"
 const locationURL = "https://www.google.com/maps/preview/locationsharing/read?gl=en&pb=%211m7%218m6%211m3%211i14%212i8413%213i5385%212i6%213x4095%212m3%211e0%212sm%213i407105169%213m7%212sen%215e1105%2112m4%211e68%212m2%211sset%212sRoadmap%214e1%215m4%211e4%218m2%211e0%211e1%216m9%211e12%212i2%2126m1%214b1%2130m1%211f1.3953487873077393%2139b1%2144e1%2150e0%2123i4111425&authuser=0&hl=en"
 
 type Location struct {
+	Name      string
 	Latitude  float64
 	Longitude float64
 }
 
 func (l Location) String() string {
-	return fmt.Sprintf("Lat: %f, Lon: %f", l.Latitude, l.Longitude)
+	return fmt.Sprintf("Lat: %f, Lon: %f, Name: %s", l.Latitude, l.Longitude, l.Name)
 }
 
 func Get(username, password string) error {
@@ -56,26 +57,33 @@ func Get(username, password string) error {
 		return errors.New("separator not found")
 	}
 
-	position, err := jsonToLocation(sdata[1])
+	positions, err := jsonToLocation(sdata[1])
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(position.String())
-
+	for _, position := range positions {
+		fmt.Println(position.String())
+	}
 	return nil
 }
 
-func jsonToLocation(data string) (Location, error) {
+func jsonToLocation(data string) ([]Location, error) {
+	var ret []Location
 	parsed := gjson.Parse(data)
-	position := Location{
-		Latitude:  parsed.Get("0.0.1.1.2").Num,
-		Longitude: parsed.Get("0.0.1.1.1").Num,
+	devices := parsed.Get("0").Array()
+	for _, device := range devices {
+		position := Location{
+			Name:      device.Get("0.3").Str,
+			Latitude:  device.Get("1.1.2").Num,
+			Longitude: device.Get("1.1.1").Num,
+		}
+		if position.Latitude == 0 || position.Longitude == 0 {
+			return ret, errors.New("could not find value in json")
+		}
+		ret = append(ret, position)
 	}
-	if position.Latitude == 0 || position.Longitude == 0 {
-		return position, errors.New("could not find value in json")
-	}
-	return position, nil
+	return ret, nil
 }
 
 func newClient() (*http.Client, error) {
@@ -156,7 +164,6 @@ func findFormInputs(body io.ReadCloser) url.Values {
 		case html.StartTagToken:
 			token := z.Token()
 			if token.Data == "form" {
-				fmt.Println(token.String())
 				inForm = true
 			} else if token.Data == "input" && inForm == true {
 				var name string
